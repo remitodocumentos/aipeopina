9257b9d705a9ebfa253a4fefa5beefcab5ad7c2e
remitodocumentos <remitodocumentos@gmail.com-asociado@github.com>
2025-10-06 10:40:19 -0500

    agregando preguntas-admin.sql

---
+++ db/preguntas-administrativas.sql
@@ -0,0 +1,97 @@
+-- Preguntas administrativas - Gestión y administración pública
+INSERT INTO preguntas_administrativas (texto, seccion_id) VALUES 
+('Eficiencia administrativa', 1),
+('Transparencia y rendición de cuentas', 1),
+('Participación ciudadana', 1),
+('Planificación estratégica', 1),
+('Gestión financiera', 1);
+
+-- Preguntas administrativas - Infraestructura y servicios públicos
+INSERT INTO preguntas_administrativas (texto, seccion_id) VALUES 
+('Agua potable y saneamiento', 2),
+('Recolección de basuras', 2),
+('Energía eléctrica', 2),
+('Alcantarillado y aguas servidas', 2),
+('Gestión de residuos solidos', 2),
+('Vías, movilidad y transporte publico ', 2),
+('Telecomunicaciones e internet', 2),
+('Infraestructura educativa', 2),
+('infraestructura de salud', 2),
+('infraestructura practica deportiva', 2),
+('infraestructura recreación familiar', 2);
+
+-- Preguntas administrativas - Desarrollo urbano y territorial
+INSERT INTO preguntas_administrativas (texto, seccion_id) VALUES 
+('Ordenamiento territorial', 3),
+('Espacio publico', 3),
+('Vivienda', 3),
+('Patrimonio y paisaje urbano', 3),
+('Zonas verdes', 3),
+('Uso del suelo', 3),
+('Zonas de parqueo', 3),
+('Urbanismo', 3);
+
+-- Preguntas administrativas - Medio ambiente y sostenibilidad
+INSERT INTO preguntas_administrativas (texto, seccion_id) VALUES 
+('Calidad del aire', 4),
+('Calidad del agua', 4),
+('Biodiversidad y áreas verdes', 4),
+('Gestión ambiental', 4),
+('Cambio climático', 4),
+('Contaminación por ruido', 4);
+
+-- Preguntas administrativas - Desarrollo social y bienestar
+INSERT INTO preguntas_administrativas (texto, seccion_id) VALUES 
+('Calidad de la salud', 5),
+('Calidad de la educación', 5),
+('Seguridad y convivencia', 5),
+('Cultura y deporte', 5),
+('Recreación y esparcimiento familiar', 5),
+('Recreación y esparcimiento adulto mayor', 5),
+('Inclusión social', 5),
+('Empleo y desarrollo económico', 5);
+
+-- Preguntas administrativas - Desarrollo económico y competitividad
+INSERT INTO preguntas_administrativas (texto, seccion_id) VALUES 
+('Innovación y tecnología', 6),
+('Emprendimiento y empresariado', 6),
+('Actividad económica licita', 6),
+('Turismo', 6),
+('Inversión extranjera', 6),
+('Uso de la inteligencia artificial', 6);
+
+-- Preguntas administrativas - Calidad de vida en general
+INSERT INTO preguntas_administrativas (texto, seccion_id) VALUES 
+('Satisfacción ciudadana', 7),
+('Salud y bienestar', 7),
+('Seguridad alimentaria', 7),
+('Empleo de calidad', 7),
+('Vivienda adecuada', 7),
+('Estabilidad económica familiar', 7),
+('Salud física', 7),
+('Salud mental', 7),
+('Ambientes saludables', 7),
+('Inclusión educativa', 7),
+('Confianza y solidaridad', 7),
+('Ambiente sano', 7),
+('Vida cultural de calidad', 7),
+('Recreación y esparcimiento de calidad', 7),
+('Seguridad humana', 7);
+
+-- Preguntas administrativas - Vulneración del bienestar social
+INSERT INTO preguntas_administrativas (texto, seccion_id) VALUES 
+('Acciones contra la violencia familiar', 8),
+('Acciones contra la violencia sexual', 8),
+('Acciones contra la violencia sexual', 8),
+('Acciones contra la drogadicción', 8),
+('Acciones contra el alcoholismo', 8),
+('Acciones contra el tabaquismo', 8),
+('Acciones contra la inseguridad ciudadana', 8),
+('Acciones contra la inseguridad vial', 8),
+('Acciones contra el desamparo de adultos mayores', 8),
+('Acciones contra la contaminación ambiental y acustica', 8),
+('Acciones contra la ocupación del espacio público', 8),
+('Acciones contra la pobreza multidimensional', 8),
+('Acciones contra la delincuencia común', 8),
+('Acciones contra la corrupción', 8),
+('Acciones contra la falta de respuesta institucional', 8);
