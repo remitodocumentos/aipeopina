34ca1d2f89ca4285ae4b2119b2314ce80a5fc416
remitodocumentos <remitodocumentos@gmail.com-asociado@github.com>
2025-10-09 16:45:45 -0500

    app.js

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/index'));
 app.use('/evaluacion', require('./routes/evaluacion'));
 app.use('/resultados', require('./routes/resultados'));
+app.use('/resultados-final', require('./routes/resultados-final'));
 app.use('/admin', require('./routes/admin'));
 
 // Manejo de errores 404 - SIMPLIFICADO
